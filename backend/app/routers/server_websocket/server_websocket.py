820d416e4bbcf3c741c491765164dbbd9222691f
bendeez <daniellacina8@gmail.com>
2024-04-27 18:07:02 -0500

    added websocket validation

--- backend/app/routers/server_websocket/server_websocket.py
+++ backend/app/routers/server_websocket/server_websocket.py
@@ -5,6 +5,7 @@ from app.core.oauth import get_websocket_user
 from app.firebase.firebase_startup import firebase_storage
 from app.db.database import get_db
 from app.models.user import Users
+from app.schemas.websocket_data import WebsocketData
 from sqlalchemy import select
 import uuid
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -24,26 +25,14 @@ async def server(token:str,websocket:WebSocket,current_user:dict = Depends(get_w
     try:
         while True:
             data = await websocket.receive_json()
-            print(data)
-            if data["chat"] == "dm":
-                data["dm"] = int(data["dm"])
-                if "text" in data:
-                    data["text"] = str(data["text"])
-                if "serverinviteid" in data:
-                    data["serverinviteid"] = int(data["serverinviteid"])
-            if data["chat"] == "server":
-                data["server"] = int(data["server"])
-                if "text" in data:
-                    data["text"] = str(data["text"])
-            if data["type"] == "file" or data["type"] == "textandfile":
-                file_type = data["filetype"]
+            data = WebsocketData(data=data).data
+            if data.type == "file" or data.type == "textandfile":
+                file_type = data.filetype
                 filename = f"{uuid.uuid4()}.{file_type}"
-                encoded_image = base64.b64decode(data["file"].split(",")[1])
+                encoded_image = base64.b64decode(data.file.split(",")[1])
                 await asyncio.to_thread(firebase_storage.child(filename).put,encoded_image)
-                data["file"] = f"https://firebasestorage.googleapis.com/v0/b/discord-83cd2.appspot.com/o/{filename}?alt=media&token=c27e7352-b75a-4468-b14b-d06b74839bd8"
-            elif data["type"] == "link":
-                data["link"] = str(uuid.uuid4())
-            await server_manager.broadcast(websocket, data, current_user,db)
+                data.file = f"https://firebasestorage.googleapis.com/v0/b/discord-83cd2.appspot.com/o/{filename}?alt=media&token=c27e7352-b75a-4468-b14b-d06b74839bd8"
+            await server_manager.broadcast(websocket, dict(data), current_user,db)
     except WebSocketDisconnect:
         server_manager.disconnect(websocket,current_user)
     except Exception as e:
---
+++ tests/test_websocket_data.py
@@ -0,0 +1,108 @@
+from pydantic import BaseModel,Base64Str,model_validator,Field
+from datetime import datetime
+from typing import Optional,Literal,Union
+from uuid import uuid4
+
+
+class DmWebsocketMessage(BaseModel):
+    chat: Literal['dm']
+    type: str
+    dm: int
+    link: Optional[str] = None
+    serverinviteid: Optional[int] = None
+    text: Optional[str] = None
+    file: Optional[Base64Str] = None
+    filetype: Optional[str] = None
+    otheruser: str
+    username: str
+    profile: str
+    date: datetime
+
+    @model_validator(mode="after")
+    def check_sent_right_data(self):
+        if self.type == "link":
+            if self.serverinviteid is None:
+                raise Exception("No serverinviteid was sent to match with the link")
+            self.link = str(uuid4())
+        elif self.type == "text":
+            if self.text is None:
+                raise Exception("No text was sent")
+        elif self.type == "file":
+            if self.file is None or self.filetype is None:
+                raise Exception("Either a file or/and filetype was not sent")
+        elif self.type == "textandfile":
+            if self.text is None or self.file is None or self.filetype is None:
+                raise Exception("Either a file or/and filetype or/and text was not sent")
+        else:
+            raise Exception("invalid message type")
+        return self
+
+
+class ServerWebsocketMessage(BaseModel):
+    chat: Literal['server']
+    type: str
+    server: int
+    announcement: Optional[str] = None
+    text: Optional[str] = None
+    file: Optional[Base64Str] = None
+    filetype: Optional[str] = None
+    username: str
+    profile: str
+    date: datetime
+
+    @model_validator(mode="after")
+    def check_sent_right_data(self):
+        if self.type == "announcement":
+            if self.announcement is None:
+                raise Exception("No announcement was sent")
+        elif self.type == "text":
+            if self.text is None:
+                raise Exception("No text was sent")
+        elif self.type == "file":
+            if self.file is None or self.filetype is None:
+                raise Exception("Either a file or/and filetype was not sent")
+        elif self.type == "textandfile":
+            if self.text is None or self.file is None or self.filetype is None:
+                raise Exception("Either a file or/and filetype or/and text was not sent")
+        else:
+            raise Exception("invalid message type")
+        return self
+
+
+class Notification(BaseModel):
+    chat: Literal['notification']
+    type: str
+    dm: int
+    sender: str
+    receiver: str
+    profile: str
+
+    @model_validator(mode="after")
+    def check_notification(self):
+        if self.type == "message":
+            pass
+        else:
+            raise Exception("invalid message type")
+        return self
+
+
+class NotificationAll(BaseModel):
+    chat: Literal['notificationall']
+    type: str
+    status: str
+    username: str
+
+    @model_validator(mode="after")
+    def check_notification(self):
+        if self.type == "status":
+            pass
+        else:
+            raise Exception("invalid message type")
+        return self
+
+
+class WebsocketData(BaseModel):
+    data: Union[DmWebsocketMessage,ServerWebsocketMessage,Notification,NotificationAll] = Field(...,discriminator='chat')
+
+data = WebsocketData(data={'chat':'notificationall','type': 'status', 'status': 'online', 'username': 'Blaziken'})
+print(data.data)
